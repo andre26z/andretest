87da5016e130f3e71659a01edd35cb643c125245
andrecactus <andre.augusto.santos@cactusgaming.net>
2024-05-28 07:53:21 -0300

    v1

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,5 @@
 <template>
-  <div class="grid grid-cols-3 gap-4">
-    <Card v-for="pokemon in pokemons" :key="pokemon.name">
-      <div>{{ pokemon.name }}</div>
-      <div>{{ pokemon.url }}></div>
-      <div>{{ pokemonDetails[pokemon.name]?.ability || "Loading.." }}</div>
-      <div>{{ pokemonDetails[pokemon.name]?.height || "Loading.." }}</div>
-
-    </Card>
+  <div>
+    <h1>oi</h1>
   </div>
 </template>
-
-<script setup>
-import Card from "/components/Card.vue";
-import { usePokemonStore } from "/stores/usePokemonStore.ts";
-import { onMounted } from "vue";
-
-const pokemonStore = usePokemonStore();
-
-onMounted(() => {
-  pokemonStore.fetchPokemon();
-});
-
-const pokemons = computed(() => pokemonStore.pokemonList);
-const pokemonDetails = computed(() => pokemonStore.pokemonDetails);
-</script>
