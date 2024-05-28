9b4af94dbfc1df75679a20f2f83850f1d717dddc
andrecactus <andre.augusto.santos@cactusgaming.net>
2024-05-28 10:05:52 -0300

    next and previous button ok

--- pages/index.vue
+++ pages/index.vue
@@ -3,29 +3,31 @@
     <form @submit.prevent="search">
       <input type="text" v-model="searchQuery" placeholder="Search Gifs" />
       <button type="submit">Search</button>
-      <div>
-        <button type="button" @click="clearSearch">Clear Search</button>
-      </div>
+      <button type="button" @click="clearSearch">Clear Search</button>
     </form>
+    <div v-if="loading">Loading...</div>
     <div v-for="gif in gifs" :key="gif.id" class="gif">
       <img :src="gif.images.fixed_width_small.url" />
     </div>
-    <button @click="previousPage">Previouse</button>
-    <button @click="nextPage">Next</button>
+    <button @click="previousPage" :disabled="pagination.page === 1">
+      Previous
+    </button>
+    <button @click="nextPage" :disabled="pagination.page * 20 >= pagination.total">
+      Next
+    </button>
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
-
 import { useGiphyStore } from "~/stores/useGiphyStore";
+import { storeToRefs } from "pinia";
 
 const store = useGiphyStore();
 const searchQuery = ref("");
 
 const search = () => {
   store.fetchGifs(searchQuery.value);
-  store.clearResults();
 };
 
 const clearSearch = () => {
@@ -33,14 +35,13 @@ const clearSearch = () => {
   store.clearResults();
 };
 
-const nextPage = () => {
-  
-}
 const { gifs, loading, pagination } = storeToRefs(store);
+
+const { nextPage, previousPage } = store;
 </script>
 
-<style>
-.gifs {
+<style scoped>
+.gif {
   padding: 3px;
 }
 </style>
--- stores/useGiphyStore.ts
+++ stores/useGiphyStore.ts
@@ -6,34 +6,31 @@ export const useGiphyStore = defineStore("giphy", {
     gifs: [],
     currentQuery: "",
     loading: false,
-    images: "",
     pagination: {
       total: 0,
-      count: 20,
-      offset: 0,
+      page: 1,
+      limit: 20,
     },
   }),
   actions: {
-    async fetchGifs(query) {
+    async fetchGifs(query = this.currentQuery) {
       this.loading = true;
       if (query) {
         this.currentQuery = query;
       }
-      this.currentQuery = query;
+
       try {
-        const response = await axios.get(
-          "https://api.giphy.com/v1/gifs/search",
-          {
-            params: {
-              api_key: "pLURtkhVrUXr3KG25Gy5IvzziV5OrZGa",
-              q: this.currentQuery,
-              limit: this.pagination.count,
-              offset: this.pagination.offset,
-            },
-          }
-        );
+        const response = await axios.get("https://api.giphy.com/v1/gifs/search", {
+          params: {
+            api_key: "pLURtkhVrUXr3KG25Gy5IvzziV5OrZGa",
+            q: this.currentQuery,
+            limit: this.pagination.limit,
+            offset: (this.pagination.page - 1) * this.pagination.limit,
+          },
+        });
+
         this.gifs = response.data.data;
-        console.log(this.gifs);
+        this.pagination.total = response.data.pagination.total_count;
       } catch (error) {
         console.log("failed to fetch the gifs", error);
       } finally {
@@ -43,24 +40,18 @@ export const useGiphyStore = defineStore("giphy", {
     clearResults() {
       this.gifs = [];
       this.currentQuery = "";
-      this.pagination.offset = 0;
+      this.pagination.page = 1;
       this.pagination.total = 0;
     },
     nextPage() {
-      if (
-        this.pagination.offset + this.pagination.count <
-        this.pagination.total
-      ) {
-        this.pagination.offset += this.pagination.count;
+      if (this.pagination.page * this.pagination.limit < this.pagination.total) {
+        this.pagination.page += 1;
         this.fetchGifs();
       }
     },
     previousPage() {
-      if (this.pagination.offset !== 0) {
-        this.pagination.offset = Math.max(
-          0,
-          this.pagination.offset - this.pagination.count
-        );
+      if (this.pagination.page > 1) {
+        this.pagination.page -= 1;
         this.fetchGifs();
       }
     },
